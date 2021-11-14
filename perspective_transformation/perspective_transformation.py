d086aa01927a5292a1678d058a51997058769edd
Jayhyuk Choi <jay@inha.edu>
2021-11-14 17:42:03 +0900

    center 좌표 구하기

--- perspective_transformation/perspective_transformation.py
+++ perspective_transformation/perspective_transformation.py
@@ -8,21 +8,56 @@ def onMouse(event, x, y, flags, param) :
     if event == cv.EVENT_LBUTTONDOWN :
         print('왼쪽 마우스 클릭 했을 때 좌표 : ', x, y)
 
-# onMouse 를 통해 얻은 좌표
-#(70,316)
-#(131,265)
-#(320,274)
-#(379,328)
+# const ROI좌표 
+#nx, ny, nw, nh
+bbox1 = [0.4046875, 0.840625, 0.503125, 0.24375]
+bbox2 = [0.623924, 0.532232, 0.503125, 0.24375]
+#x1,y1
+#x2,y2
+#w, h
+#norm (nx,ny) = (((x1+x2)/2)/w) , ((y1+y2)/2)/h))
+#nx = ((x1+x2)/2)/w
 
+#nw = (x2 - x1)/w
+#nh = (y2 - y1)/h
+
+# w * nx * 2 = x1 + x2 = ax/ 
+# /2 = c_x 
+# h * ny * 2 = y1 + y2 = ay /2 = c_y
+
+# (ax - bx)/2 = x1
+# (ax + bx)/2 = x2  
+# (ay - by)/2 = y1
+# (ay + by)/2 = y2
+
+x1,y1 x2,y2
+#ROI 정보 
+x1 =
+x2 =
+x3 =
+x4 =
+y1 =
+y2 =
+
+def find_box_center(bbox, img):
+    width = img.cols
+    height = img.rows
+    cx = width * bbox[0]
+    cy = height * bbox[1] 
+    c_position = (cx,cy)
+    return c_position
+    
 # perspective transfer
 def bird_eye_view(frame):
     # pts1, pts2 is ROI
-    pts1 = np.float32([[131, 265], [320, 274], [70, 316], [379, 328]])
+    ROI = np.float32([[x1, y1], [x2, y1], [x3, y2], [x4, y2]])
     #pts1 = np.float32([[230, 150], [370, 150], [20, 280], [580, 280]])
-    pts2 = np.float32([[0, 0], [x_size, 0], [0, y_size], [x_size, y_size]])
-    matrix = cv.getPerspectiveTransform(pts1, pts2)
-    matrix_inv = cv.getPerspectiveTransform(pts2, pts1)
+    T_ROI = np.float32([[0, 0], [x_size, 0], [0, y_size], [x_size, y_size]])
+    matrix = cv.getPerspectiveTransform(ROI, pts2)
+    print("matrix\n",matrix)
+    matrix_inv = cv.getPerspectiveTransform(pts2, ROI)
     frame = cv.warpPerspective(frame, matrix, (x_size, y_size))
+    
     return frame
 
 # scharr_filter
