d68a25051fbd6cb6a28d89cf00ce5dc884cd0c08
seoyoung <yoon3435@inha.edu>
2021-11-28 10:13:50 +0900

    주석

--- sort.py
+++ sort.py
@@ -34,7 +34,7 @@ from filterpy.kalman import KalmanFilter
 
 np.random.seed(0)
 
-#for hungarian algorithm
+#for hungarian algorithm : 헝가리안 알고리즘
 def linear_assignment(cost_matrix):
   try:
     import lap
@@ -49,6 +49,7 @@ def linear_assignment(cost_matrix):
 def iou_batch(bb_test, bb_gt):
   """
   From SORT: Computes IOU between two bboxes in the form [x1,y1,x2,y2]
+  두 바운딩 박스 사이에 IOU 계산하기
   """
   bb_gt = np.expand_dims(bb_gt, 0)
   bb_test = np.expand_dims(bb_test, 1)
@@ -71,22 +72,22 @@ def convert_bbox_to_z(bbox):
   z = [center x,y 좌표 , 박스 크기, 너비/높이 비율]
   
   """
-  w = bbox[2] - bbox[0]
-  h = bbox[3] - bbox[1]
-  x = bbox[0] + w/2.
-  y = bbox[1] + h/2.
-  s = w * h    #scale is just area
-  r = w / float(h)
+  w = bbox[2] - bbox[0] # x2 - x1
+  h = bbox[3] - bbox[1] # y2 - y1
+  x = bbox[0] + w/2. # 중심 좌표 x
+  y = bbox[1] + h/2. # 중심 좌표 y
+  s = w * h    # 바운딩 박스 스케일 (크기)
+  r = w / float(h) # 가로 세로 비율
   return np.array([x, y, s, r]).reshape((4, 1))
 
 
 def convert_x_to_bbox(x,score=None):
   """
-  z 정보를 통해 bbox 왼쪽 위 오른쪽 아래 좌표 변환
+  [x, y, s, r] -> [x1, y1, x2, y2]
   """
-  w = np.sqrt(x[2] * x[3])
-  h = x[2] / w
-  if(score==None):
+  w = np.sqrt(x[2] * x[3]) # s*r=w*h*w/h = w^2
+  h = x[2] / w # w*h/w
+  if(score==None): # score가 없다면
     return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.]).reshape((1,4))
   else:
     return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.,score]).reshape((1,5))
@@ -95,18 +96,28 @@ def convert_x_to_bbox(x,score=None):
 class KalmanBoxTracker(object):
   """
   This class represents the internal state of individual tracked objects observed as bbox.
+  바운딩 박스로 관찰된 tracked objected의 상태를 나타냄
+  tracking : 어떤 object의 물체의 위치를 찾는 과정
+  tracker : 객체 정보
   """
   count = 0
   def __init__(self,bbox):
     """
     Initialises a tracker using initial bounding box.
+    초기 바운딩 박스 값을 사용하여 tracker를 초기화 한다. (object 정보)
     """
-    #define constant velocity model
-    #dim_x : size of the state vector 
-    #dim_z : size of the measurement vector
-    self.kf = KalmanFilter(dim_x=7, dim_z=4)
+    #define constant velocity model 
+    #dim_x : size of the state vector : 상태 벡터의 수
+    #dim_z : size of the measurement vector : 측정 벡터의 수
+    # 시스템의 상태로부터 측정값을 추정하는 것이 목표
+
+
+    self.kf = KalmanFilter(dim_x=7, dim_z=4) # 상태 벡터 7개 측정벡터 4개
+    # x = [u, v, s, r, u', v', s']T => 개체의 무엇을 추적하고 있는지
+    # 측정 벡터 [x1, y1, x2, y2] => 4개
     
     #  F : ndarray (dim_x, dim_x) /state transistion matrix
+    #  F : 일부 식에서는 F가 아닌 A로 나와있기도 함. 시스템 행렬 - 상태 전이 행렬
     self.kf.F = np.array([[1,0,0,0,1,0,0],
                           [0,1,0,0,0,1,0],
                           [0,0,1,0,0,0,1],
@@ -115,29 +126,34 @@ class KalmanBoxTracker(object):
                           [0,0,0,0,0,1,0],
                           [0,0,0,0,0,0,1]])
     # H : ndarray (dim_z, dim_x) /measurement function
+    # H : 측정 기능 / 측정값과 상태변수의 관계
     self.kf.H = np.array([[1,0,0,0,0,0,0],
                           [0,1,0,0,0,0,0],
                           [0,0,1,0,0,0,0],
                           [0,0,0,1,0,0,0]])
     # R : ndarray (dim_z, dim_z), default eye(dim_x) /measurement uncertainty/noise
+    # R : 측정 불확실성, 잡음 행렬
     self.kf.R[2:,2:] *= 10.
     # P : ndarray (dim_x, dim_x), default eye(dim_x) /covariance matrix
+    # P : 공분산 행렬
     self.kf.P[4:,4:] *= 1000. #give high uncertainty to the unobservable initial velocities
     self.kf.P *= 10.
+    # P가 크면 추정 오차가 크고 P가 작으면 추정 오차가 작다 => 높은 불확실성 부여 (초기)
+
     #Q : ndarray (dim_x, dim_x), default eye(dim_x) /Process uncertainty/noise
+    #Q : 프로세스의 불확실성, 노이즈
     self.kf.Q[-1,-1] *= 0.01
     self.kf.Q[4:,4:] *= 0.01
     #x : ndarray (dim_x, 1), default = [0,0,0…0] /filter state estimate
+    #상태변수 (관심있는 값)
     self.kf.x[:4] = convert_bbox_to_z(bbox) # [x,y,s,r]
-    
-    # stage 변경시 count 증가
-    self.time_since_update = 0
-    self.id = KalmanBoxTracker.count
-    KalmanBoxTracker.count += 1
-    self.history = []
-    self.hits = 0
+    self.time_since_update = 0 # update부터 지난 시간
+    self.id = KalmanBoxTracker.count # 오브젝트 아이디
+    KalmanBoxTracker.count += 1 # count 값 1 증가 (object 개수?)
+    self.history = [] # history 배열
+    self.hits = 0 # update 수
     self.hit_streak = 0
-    self.age = 0
+    self.age = 0 # 예측 진행 수
 
   def update(self,bbox):
     """
@@ -145,27 +161,30 @@ class KalmanBoxTracker(object):
     """
     self.time_since_update = 0
     self.history = []
-    self.hits += 1
-    self.hit_streak += 1
-    self.kf.update(convert_bbox_to_z(bbox))
+    self.hits += 1 # update 수
+    self.hit_streak += 1 # 이게 뭘까 ******************************
+    self.kf.update(convert_bbox_to_z(bbox)) # update 함수 이용 
+    # 칼만필터의 새 측정 z를 추가한다.
 
   def predict(self):
     """
     Advances the state vector and returns the predicted bounding box estimate.
     """
-    if((self.kf.x[6]+self.kf.x[2])<=0):
-      self.kf.x[6] *= 0.0
-    self.kf.predict()
-    self.age += 1
-    if(self.time_since_update>0):
+    if((self.kf.x[6]+self.kf.x[2])<=0): # 바운딩 박스의 크기 + 크기' <= 0
+      self.kf.x[6] *= 0.0 # s'를 모두 0으로
+    self.kf.predict() # predict 함수 이용
+    self.age += 1 # 예측 진행
+    if(self.time_since_update>0): # 방금 막 업데이트 되지 않음
       self.hit_streak = 0
-    self.time_since_update += 1
+    self.time_since_update += 1 # 업데이트 이후 지난 시간 1 증가
     self.history.append(convert_x_to_bbox(self.kf.x))
-    return self.history[-1]
+    # history 배열에 예측값 저장
+    return self.history[-1] # 마지막 요소 반환
 
   def get_state(self):
     """
     Returns the current bounding box estimate.
+    추정치 반환
     """
     return convert_x_to_bbox(self.kf.x)
 
