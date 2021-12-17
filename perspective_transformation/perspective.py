1fd5df8a5ed1720a7c69ffb1d4673cbcb380c0e0
seoyoung <yoon3435@inha.edu>
2021-12-17 11:08:21 +0900

    pespective 콘 좌표 수정

--- perspective_transformation/perspective.py
+++ perspective_transformation/perspective.py
@@ -7,14 +7,14 @@ width = 1920
 height = 1080
 
 def find_distance(bbox):
-
     cx = bbox[0] + (bbox[2]/2)
     cy = bbox[1] + (bbox[3]/2)
     c_position = (cx,cy, 1)
     
     # pts1, pts2 is ROI
     # pts1은 임의로 지정한 4개의 콘 영역이다
-    pts1 = np.float32([[457, 279], [612, 282], [147, 720], [955, 720]]) # 이거 1080때 인지 기억이 안납니다!! 1080 720 때면 전환해야합니다
+    # [457, 279], [612, 282], [147, 720], [955, 720]
+    pts1 = np.float32([[812, 419], [1088, 423], [216, 1080], [1698, 1080]])
     pts2 = np.float32([[0, 0], [width, 0], [0,height], [width, height]])
     # ROI 사이의 homography 
     matrix = cv.getPerspectiveTransform(pts1, pts2)
